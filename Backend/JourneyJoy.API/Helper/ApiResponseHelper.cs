40b72f8e7633687d51524af3fc4eb028b3a821a1
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-07-16 21:18:25 +0300

    Helper folder ..

---
+++ Backend/JourneyJoy.API/Helper/ApiResponseHelper.cs
@@ -0,0 +1,18 @@
+﻿using JourneyJoy.DTO.ServiceResponseDtos;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace JourneyJoy.API.Helper
+{
+    public class ApiResponseHelper
+    {
+        public static IActionResult CreateApiResponse<T>(ApiResponseDto<T> response)
+        {
+            if (response.Success)
+                return new OkObjectResult(response);
+
+            Log.Error("Error: {Message}", response.Message);
+            return new ObjectResult(response) { StatusCode = response.StatusCode };
+        }
+    }
+}
---
+++ Backend/JourneyJoy.API/Helper/ValidationFilterHelper.cs
@@ -0,0 +1,27 @@
+﻿using JourneyJoy.DTO.ServiceResponseDtos;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JourneyJoy.API.Helper
+{
+    public class ValidationFilterHelper : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .ToDictionary(
+                        kvp => kvp.Key,
+                        kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToList()
+                    );
+
+                var response = new ApiResponseDto<string>(null, false, 400, "Validation Errors", errors);
+                context.Result = new BadRequestObjectResult(response);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
+    }
+}
